1a3132bc0975f0deb011171403e42b49521c5c35
Oleksandr Burdeinyi <aburdeinyi@cybervisiontech.com>
2017-01-27 21:15:56 +0200

    JEK-10 moved sources from  gh-pages-stub to src folder

--- jekver.rb
+++ jekver.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 require 'fileutils'
-require "./#{File.dirname(__FILE__)}/gh-pages-stub/_scripts/tst_deploy.rb"
+require "./#{File.dirname(__FILE__)}/src/tst_deploy.rb"
 
 begin
     dp = Deploy.new(path = ARGV[0])
