e6cdb7f570507db00ca55f429ff15bfff31e72da
tmakhmudov <tmakhmudov@cybervisiontech.com>
2018-02-27 16:07:39 +0200

    Fixed to work with syntax highlight, added comments and meaningful var/func names

--- gh-pages-stub/public/js/snippets.js
+++ gh-pages-stub/public/js/snippets.js
@@ -1,43 +1,68 @@
 "use strict";
 
-var snipArr, arrl, visibleArr, wholeArr;
-snipArr = document.getElementsByTagName("CODE");
-arrl = snipArr.length;
-visibleArr = [];
-wholeArr = [];
-cutAll();
+var codeElementsArray, snippetCount, shortenedSnippetsArray, uncutSnippetsArray;
+codeElementsArray = document.getElementsByTagName("CODE");
+snippetCount = codeElementsArray.length;
+shortenedSnippetsArray = [];
+uncutSnippetsArray = [];
+processAllSnippets();
 
-function cutAll() {
-  var i, delim;
-  for (i=0; i<arrl; i++) {
-    delim = snipArr[i].innerHTML.indexOf("&gt;&gt;&gt;showmore");
-    wholeArr[i] = snipArr[i].innerHTML;
-    if (delim === -1) {continue;}
-    visibleArr[i] = snipArr[i].innerHTML.slice(0,delim);
-    snipArr[i].parentElement.innerHTML += "<button class='collapse_control more collapsed' id='showmoreId" + i + "' onclick='showMore()'>Show more</button>";
-    snipArr[i].innerHTML = visibleArr[i];
+function processAllSnippets() {
+  var i, snippetDivider;
+  for (i=0; i<snippetCount; i++) {
+
+    // Define position of line containing delimiter -- that line will be dividing point for snippet
+    snippetDivider = codeElementsArray[i].innerHTML.search(/.*``*./);
+
+    uncutSnippetsArray[i] = codeElementsArray[i].innerHTML;
+    if (snippetDivider === -1) {continue;}
+
+    //Create array of processed snippets (shorten them all by the line containing delimiter)
+    shortenedSnippetsArray[i] = codeElementsArray[i].innerHTML.slice(0,snippetDivider);
+
+    //Add 'Show more' button to the parent <PRE> element of the <CODE> element containing snippet
+    codeElementsArray[i].parentElement.innerHTML += "<button class='collapse_control more collapsed' id='showmoreId" + i + "' onclick='showMore()'>Show more</button>";
+
+    //Render page making all snippets processed
+    codeElementsArray[i].innerHTML = shortenedSnippetsArray[i];
   }
 }
 
 function showMore() {
-  var x, y, z, start, end, whole, parentPre;
-  parentPre = event.target.parentElement;
-  x = event.target.id.slice(10);
-  y = wholeArr[x].indexOf("&gt;&gt;&gt;showmore");
-  start = wholeArr[x].slice(0,y);
-  z = y + 21;
-  end = wholeArr[x].slice(z);
-  whole = start + end;
-  parentPre.firstChild.innerHTML = whole;
-  event.target.outerHTML = "<button class='collapse_control' id='showlessId" + x + "' onclick='showLess()'>Show less</button>";
+  var buttonId, delimiterStart, delimiterEnd, snippetStart, snippetEnd, completeSnippet, buttonContainer;
+
+  //Parent <PRE> element of the <CODE> element. <PRE> contains 'Show more' button, <CODE> contains snippet
+  buttonContainer = event.target.parentElement;
+
+  //Extract 'Show more' button id number
+  buttonId = event.target.id.slice(10);
+
+  delimiterStart = uncutSnippetsArray[buttonId].indexOf("``");
+  snippetStart = uncutSnippetsArray[buttonId].slice(0,delimiterStart);
+
+  //Define position of delimiter end inside snippet (our delimiter is 2 characters long)
+  delimiterEnd = delimiterStart + 2;
+
+  snippetEnd = uncutSnippetsArray[buttonId].slice(delimiterEnd);
+  completeSnippet = snippetStart + snippetEnd;
+  buttonContainer.firstChild.innerHTML = completeSnippet;
+
+  //Change 'Show more' button to 'Show less'
+  event.target.outerHTML = "<button class='collapse_control' id='showlessId" + buttonId + "' onclick='showLess()'>Show less</button>";
 }
 
 function showLess() {
-  var a, pos, complete, parPre;
-  parPre = event.target.parentElement;
-  a = event.target.id.slice(10);
-  pos = wholeArr[a].indexOf("&gt;&gt;&gt;showmore");
-  complete = wholeArr[a].slice(0,pos);
-  parPre.firstChild.innerHTML = complete;
-  event.target.outerHTML = "<button class='collapse_control more collapsed' id='showmoreId" + a + "' onclick='showMore()'>Show more</button>";
+  var buttonId, snippetDivider, shortenedSnippet, buttonContainer;
+
+  buttonContainer = event.target.parentElement;
+
+  buttonId = event.target.id.slice(10);
+
+  snippetDivider = uncutSnippetsArray[buttonId].search(/.*``*./);
+
+  shortenedSnippet = uncutSnippetsArray[buttonId].slice(0,snippetDivider);
+
+  buttonContainer.firstChild.innerHTML = shortenedSnippet;
+
+  event.target.outerHTML = "<button class='collapse_control more collapsed' id='showmoreId" + buttonId + "' onclick='showMore()'>Show more</button>";
 }
